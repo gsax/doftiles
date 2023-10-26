a32e93eb77c889f0c184e219fb82fbf23c3694b1
Georg Sachs <gsx@protaisn.org>
2023-10-26 19:17:56 +0200

    Add transparency to emacs

--- emacs/doom/config.el
+++ emacs/doom/config.el
@@ -42,6 +42,9 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
+;; Set transparent background
+(set-frame-parameter nil 'alpha-background 90)
+(add-to-list 'default-frame-alist '(alpha-background . 90))
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
